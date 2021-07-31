53f46904822ecbefe659db88a283db67d4f6ae82
sridharmkumar <sridhar.muthukumar.in@gmail.com>
2021-07-31 13:06:20 +0530

    Dynamic HBase Catalog & Spark Text File Streaming

--- src/main/scala/com/spark/hive/DynamicHBaseHiveIntegration.scala
+++ src/main/scala/com/spark/hive/DynamicHBaseHiveIntegration.scala
@@ -1,7 +1,8 @@
-package com.spark.hive
+package com.spark.hbase
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object DynamicHBaseHiveIntegration {
@@ -12,9 +13,12 @@ object DynamicHBaseHiveIntegration {
     sc.setLogLevel("ERROR")
 
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+    val hive = new HiveContext(sc)
+
     val columnData = spark.read.format("csv").option("header", "true").load("file:///home/cloudera/data/columns.csv")
     println()
-    println("****** Column Mapping Data ******")
+    println("****** Column Mapping Details ******")
     println()
     columnData.show(false)
 
@@ -34,16 +38,21 @@ object DynamicHBaseHiveIntegration {
     }
     middle = middle.trim().dropRight(1)
     val suffix = System.lineSeparator() + "}" + System.lineSeparator() + "}"
-
-    println()
-    println("****** HBase Catalog ******")
     val catalog = prefix + middle + suffix
+    println()
+    println("****** Generated HBase Catalog ******")
     println(catalog)
 
     println()
     println("****** Reading Data from HBase ******")
     println()
     val hbaseData = spark.read.options(Map(HBaseTableCatalog.tableCatalog -> catalog)).format("org.apache.spark.sql.execution.datasources.hbase").load()
+
+    println("HBase Schema:")
+    hbaseData.printSchema()
+
+    println()
+    println("HBase Records:")
     hbaseData.show(5, truncate = false)
 
     println()
@@ -51,9 +60,16 @@ object DynamicHBaseHiveIntegration {
     println()
     val derivedTableNames = columnDataAsList.map(x => x.getString(1).split("_")(0)).distinct
     for (derivedVal <- derivedTableNames) {
-      val columnNames = hbaseData.columns.filter(x => x.contains("") || x.startsWith(derivedVal))
+      val columnNames = hbaseData.columns.filter(x => x.contains("masterid") || x.startsWith(derivedVal))
       val finalTableName = "project_4." + derivedVal.concat("_tab")
-      hbaseData.select(columnNames.head, columnNames.tail: _*).write.mode("overwrite").format("hive").saveAsTable(finalTableName)
+      println("Writing Data for table - " + finalTableName)
+
+      val finalData = hbaseData.select(columnNames.head, columnNames.tail: _*)
+      println("Schema:")
+      finalData.printSchema()
+
+      finalData.write.mode("overwrite").format("hive").saveAsTable(finalTableName)
+      println()
     }
     println()
     println("****** Data Written Successfully! ******")
